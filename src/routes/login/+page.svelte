d891759eb71f6fd4ef81a589c9a24040368cbf2c
Morty <42971559+evil1morty@users.noreply.github.com>
2024-08-09 23:30:15 +0100

    other pages and fonts file

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+	import { enhance } from '$app/forms'
+	import Turnstile from '$lib/components/shared/Turnstile.svelte'
+	import { currentPage } from '$lib/stores/general'
+
+	currentPage.set('Login')
+
+	/** @type {import('./$types').ActionData} */
+	export let form
+	/** @type {Turnstile} */
+	let turnstileComponent
+</script>
+
+<div class="flex items-center justify-center mt-20">
+	<div class="card w-full max-w-md p-6 pb-0 mx-2 rounded shadow-lg">
+		<h1 class="text-2xl font-bold mb-6 text-center">Login</h1>
+		<form method="post" use:enhance>
+			<div class="mb-4">
+				<label for="username" class="label mb-2 text-sm font-medium">Username</label>
+				<input type="text" name="username" class="input" placeholder="Enter your username" minlength="3" maxlength="24" required />
+			</div>
+			<div class="mb-6">
+				<label for="password" class="block mb-2 text-sm font-medium">Password</label>
+				<input type="password" name="password" class="input" placeholder="Enter your password" minlength="6" required />
+			</div>
+			<button class="hidden" id="submit" type="submit"></button>
+			<Turnstile bind:this={turnstileComponent} />
+			<div class="flex flex-col items-center space-y-3">
+				<button
+					on:click={() => {
+						turnstileComponent.render()
+					}}
+					type="button"
+					class="btn variant-filled-primary font-medium rounded-lg px-4 py-2 w-full">Login</button
+				>
+			</div>
+			{#if form?.message}
+				<h3 id="message" class="alert-message text-center text-warning-600 mt-4">{form.message}</h3>
+			{/if}
+			<footer class="card-footer text-center mt-4">
+				Don't have an account? <a href="/sign-up" class="underline text-primary-600 dark:text-primary-100">Sign up</a>
+			</footer>
+		</form>
+	</div>
+</div>
