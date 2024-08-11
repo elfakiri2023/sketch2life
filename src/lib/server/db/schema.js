7efca10233eeb1e1f082a22d32f50fd67d0bb4b5
Morty <42971559+evil1morty@users.noreply.github.com>
2024-08-11 20:34:15 +0100

    add sketch column to the database shema

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,24 +1,25 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const userTable = sqliteTable('user', {
-    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    username: text('username').notNull().unique(),
-    email: text('email').notNull().unique(),
-    password: text('password').notNull(),
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+	username: text('username').notNull().unique(),
+	email: text('email').notNull().unique(),
+	password: text('password').notNull()
 })
 
 export const savedTable = sqliteTable('saved', {
-    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
-    name: text('name'),
-    user_id: text('user_id')
-        .notNull()
-        .references(() => userTable.id, { onDelete: 'cascade' })
+	id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+	image_name: text('image_name'),
+	sketch: text('sketch'),
+	user_id: text('user_id')
+		.notNull()
+		.references(() => userTable.id, { onDelete: 'cascade' })
 })
 
 export const sessionTable = sqliteTable('session', {
-    id: text('id').notNull().primaryKey(),
-    userId: text('user_id')
-        .notNull()
-        .references(() => userTable.id),
-    expiresAt: integer('expires_at').notNull()
+	id: text('id').notNull().primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => userTable.id),
+	expiresAt: integer('expires_at').notNull()
 })
