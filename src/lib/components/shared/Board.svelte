151719adaee13ffa0a9e3439a30c51e64b89ee53
Morty <42971559+evil1morty@users.noreply.github.com>
2024-08-12 19:33:12 +0100

    prettier

--- src/lib/components/shared/Board.svelte
+++ src/lib/components/shared/Board.svelte
@@ -7,8 +7,12 @@
 	let lastDistance = null
 
 	function initDraw(e) {
-		if ($tool !== 'pen' && $tool !== 'eraser') return
+		if ($tool !== 'pen' && $tool !== 'eraser') {
+			return
+		}
+
 		$isDrawing = true
+
 		const stage = e.detail.target.getStage()
 		const pos = relativePointerPosition(stage)
 		lines.update((ls) => [...ls, { tool: $tool, points: [pos.x, pos.y] }])
@@ -31,8 +35,14 @@
 	}
 
 	function handleDraw(e) {
-		if ($tool !== 'pen' && $tool !== 'eraser') return
-		if (!$isDrawing) return
+		if ($tool !== 'pen' && $tool !== 'eraser') {
+			return
+		}
+
+		if (!$isDrawing) {
+			return
+		}
+
 		const stage = e.detail.target.getStage()
 		const point = relativePointerPosition(stage)
 		lines.update((ls) => {
@@ -44,7 +54,10 @@
 	}
 
 	function stopDraw(e) {
-		if ($tool !== 'pen' && $tool !== 'eraser') return
+		if ($tool !== 'pen' && $tool !== 'eraser') {
+			return
+		}
+
 		$isDrawing = false
 		generatePreview(e)
 	}
@@ -57,18 +70,23 @@
 	}
 
 	function handleZoom(e) {
-		if ($tool !== 'grab') return
-		const scaleBy = 1.3
+		if ($tool !== 'grab') {
+			return
+		}
+
 		e.evt.preventDefault()
+
+		const scaleBy = 1.3
 		const stage = e.detail.target.getStage()
-		let oldScale = stage.scaleX()
-		let mousePointTo = {
+		const oldScale = stage.scaleX()
+		const mousePointTo = {
 			x: stage.getPointerPosition().x / oldScale - stage.x() / oldScale,
 			y: stage.getPointerPosition().y / oldScale - stage.y() / oldScale
 		}
-		let newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy
+		const newScale = e.evt.deltaY < 0 ? oldScale * scaleBy : oldScale / scaleBy
 		stage.scale({ x: newScale, y: newScale })
-		let newPos = {
+
+		const newPos = {
 			x: -(mousePointTo.x - stage.getPointerPosition().x / newScale) * newScale,
 			y: -(mousePointTo.y - stage.getPointerPosition().y / newScale) * newScale
 		}
@@ -76,32 +94,39 @@
 	}
 
 	function handlePinchZoom(e) {
-		if ($tool !== 'grab') return
+		if ($tool !== 'grab') {
+			return
+		}
+
 		const stage = e.detail.target.getStage()
 		const touch1 = e.evt.touches[0]
 		const touch2 = e.evt.touches[1]
-		if (!touch1 || !touch2) return
-		if (stage.isDragging()) stage.stopDrag()
+
+		if (!touch1 || !touch2) {
+			return
+		}
+
+		if (stage.isDragging()) {
+			stage.stopDrag()
+		}
+
 		const point1 = { x: touch1.clientX, y: touch1.clientY }
 		const point2 = { x: touch2.clientX, y: touch2.clientY }
-		if (!lastCenter) lastCenter = getCenter(point1, point2)
+
+		if (!lastCenter) {
+			lastCenter = getCenter(point1, point2)
+		}
+
 		const newCenter = getCenter(point1, point2)
 		const dist = getDistance(point1, point2)
-		if (!lastDistance) lastDistance = dist
-		let pointTo = {
-			x: (newCenter.x - stage.x()) / stage.scaleX(),
-			y: (newCenter.y - stage.y()) / stage.scaleX()
+
+		if (!lastDistance) {
+			lastDistance = dist
 		}
+
 		let scale = stage.scaleX() * (dist / lastDistance)
 		stage.scaleX(scale)
 		stage.scaleY(scale)
-		if (!lastCenter) return
-		let dx = newCenter.x - lastCenter.x
-		let dy = newCenter.y - lastCenter.y
-		let newPos = {
-			x: newCenter.x - pointTo.x * scale + dx,
-			y: newCenter.y - pointTo.y * scale + dy
-		}
 	}
 
 	function stopPinchZoom(e) {
@@ -160,7 +185,7 @@
 						key: i,
 						points: line.points,
 						stroke: $color,
-						strokeWidth: line.tool === 'eraser' ? 20 : 2,
+						strokeWidth: line.tool === 'eraser' ? 18 : 2,
 						tension: 0.2,
 						lineCap: 'round',
 						lineJoin: 'round',
