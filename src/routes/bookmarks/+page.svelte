bacfab8a383ea2e383acd9996cd8680e4db63be9
Morty <42971559+evil1morty@users.noreply.github.com>
2024-08-13 16:52:15 +0100

    bookmarks page navbar

--- src/routes/bookmarks/+page.svelte
+++ src/routes/bookmarks/+page.svelte
@@ -1,1 +1,16 @@
-bookmarks page
+<script>
+	import Navbar from '$lib/components/ui/Navbar.svelte'
+</script>
+
+<Navbar />
+<div class="border-t-2 border-gray-200" />
+
+<div class="container mx-auto px-10 my-10">
+	<section class="grid grid-cols-2 md:grid-cols-3 gap-6">
+		{#each { length: 10 } as _, i}
+			<div>
+				<img class="h-auto max-w-full rounded-lg" src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=512&h=512&auto=format&fit=crop" alt="" />
+			</div>
+		{/each}
+	</section>
+</div>
